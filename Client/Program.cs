d3623038b1d3d5ea946106ce8edeee240036895a
Mats Bergström <matsblix@outlook.com>
2021-12-27 19:57:24 +0100

    Första versionen av layouten med exempelinnehåll

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,8 +1,20 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services
+      .AddBlazorise( options =>
+      {
+          options.ChangeTextOnKeyPress = true;
+      } )
+      .AddBootstrapProviders()
+      .AddFontAwesomeIcons();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
