47b3165a7fb714931ab6fd1ea68e7869a9c1a31e
Mats Bergström <mats.bergstrom@nordicport.onmicrosoft.com>
2022-03-03 19:09:07 +0100

    Första version som sparar tips men bara lokalt just nu.

---
+++ Client/Models/BetModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp.Client.Models
+{
+    public class BetModel
+    {        
+        public string DriverId { get; set; } = string.Empty;
+        public int Position { get; set; }
+    }
+}
