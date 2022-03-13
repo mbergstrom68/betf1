7fb952ff063d007943185f5f74d6aced6be2fe83
Mats Bergström <mats.bergstrom@nordicport.onmicrosoft.com>
2022-03-13 13:51:10 +0100

    Added driver position number in driver listing (needs styling). Updated date and time format on race listing.

---
+++ Client/Models/BettingDriverModel.cs
@@ -0,0 +1,10 @@
+﻿using ErgastApi.Responses.Models;
+
+namespace BlazorApp.Client.Models
+{
+    public class BettingDriverModel
+    {
+        public int Position { get; set; }
+        public Driver? DriverInfo { get; set; }
+    }
+}
